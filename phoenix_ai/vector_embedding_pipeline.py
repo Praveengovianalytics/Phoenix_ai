48aa44ab89a0f6c205b05b2fcad64fa27f1fc2c5
Praveen Govindaraj <38414524+Praveengovianalytics@users.noreply.github.com>
2025-06-02 17:34:10 +0800

    Update vector_embedding_pipeline.py

--- phoenix_ai/vector_embedding_pipeline.py
+++ phoenix_ai/vector_embedding_pipeline.py
@@ -6,7 +6,7 @@ import pandas as pd
 from databricks.vector_search.client import VectorSearchClient
 
 class VectorEmbedding:
-    def __init__(self, embedding_client):
+    def __init__(self, embedding_client,chunk_size,overlap):
         self.client = embedding_client
         self.embedding_model = embedding_client.model
         self.chunk_size = chunk_size
@@ -16,9 +16,9 @@ class VectorEmbedding:
         chunks = []
         start = 0
         while start < len(text):
-            end = start + chunk_size
+            end = start + self.chunk_size
             chunks.append(text[start:end])
-            start = end - overlap
+            start = end - self.overlap
         return chunks
 
     def _generate_embeddings(self, chunks: list) -> list:
